cea398636b1e90cebd75e74357ea8b03851edf36
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2018-11-29 14:41:07 -0800

    Add remaining dependencies. Switch to CMake.

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import CMake, ConanFile, tools
 import os
 
 # https://github.com/giorgioazzinnaro/rocksdb/blob/master/INSTALL.md
 
 class RocksdbConan(ConanFile):
     name = "RocksDB"
-    version = "5.8"
+    version = "5.17.2"
     license = "https://github.com/facebook/rocksdb/blob/master/COPYING"
-    url = "https://github.com/giorgioazzinnaro/conan-RocksDB"
+    url = "https://github.com/ambroff/conan-RocksDB"
     description = "A library that provides an embeddable, persistent key-value store for fast storage."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
@@ -15,58 +15,42 @@ class RocksdbConan(ConanFile):
     generators = "cmake"
 
     source_tgz = "https://github.com/facebook/rocksdb/archive/v%s.tar.gz" % version
+    checksum = '101f05858650a810c90e4872338222a1a3bf3b24de7b7d74466814e6a95c2d28'
 
     requires = (
         "zlib/1.2.11@conan/stable",
         "bzip2/1.0.6@conan/stable",
-        "LZ4/1.8.0@bincrafters/stable"
-        # TODO snappy, zstandard
+        "lz4/1.8.3@bincrafters/stable",
+        "snappy/1.1.7@bincrafters/stable",
+        "zstd/1.3.5@bincrafters/stable",
+        "gflags/2.2.1@bincrafters/stable",
+        "glog/0.3.5@bincrafters/stable",
     )
 
     def source(self):
         self.output.info("Downloading %s" %self.source_tgz)
-        tools.download(self.source_tgz, "rocksdb.tar.gz")
-
-        tools.unzip("rocksdb.tar.gz")
-        os.remove("rocksdb.tar.gz")
+        tools.get(self.source_tgz, sha256=self.checksum)
 
     @property
     def subfolder(self):
         return "rocksdb-%s" % self.version
 
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(
+            source_folder='rocksdb-{}'.format(self.version),
+            defs={'CMAKE_POSITION_INDEPENDENT_CODE': True})
+        return cmake
+    
     def build(self):
-        if tools.os_info.is_windows:
-            self.windows_build()
-        else:
-            self.unix_build()
-
-    def windows_build(self):
-        # TODO
-        True
-
-    def unix_build(self):
-        with tools.chdir(self.subfolder):
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.fpic = True
-
-            if self.settings.build_type == "Debug":
-                env_build.make()  # $ make
-            else:
-                if self.options.shared:
-                    env_build.make(["shared_lib"])  # $ make shared_lib
-                else:
-                    env_build.make(["static_lib"])  # $ make static_lib
+        cmake = self.configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy("LICENSE.Apache", src=self.subfolder, keep_path=False)
         self.copy("LICENSE.leveldb", src=self.subfolder, keep_path=False)
-
-        self.copy("*.h", dst="include", src=("%s/include" % self.subfolder))
-
-        if self.options.shared:
-            self.copy("librocksdb.so", dst="lib", src=self.subfolder, keep_path=False)
-        else:
-            self.copy("librocksdb.a", dst="lib", src=self.subfolder, keep_path=False)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["rocksdb"]
